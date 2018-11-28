8fb84cf30f114100c3b2e1f0e15d42534e26c4fb
hbynlsl <hbynlsl@gmail.com>
2018-11-28 13:45:44 +0800

    修改readme文件

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,31 @@
 // vue-webpack配置 
 module.exports = {
-    outputDir: "./public/static/",
-    baseUrl: process.env.baseUrl,
-    pages: {
-        index: {
-            entry: 'src/index.js',
-            filename: process.env.htmlPath + 'index.html'
-        },
-        admins: {
-            entry: 'src/admins.js',
-            filename: process.env.htmlPath + 'admins.html'
-        }
+  outputDir: "./public/static/",
+  baseUrl: process.env.baseUrl,
+  pages: {
+    index: {
+      entry: "src/index.js",
+      filename: process.env.htmlPath + "index.html"
     },
-    devServer: {
-        port: 8000,
-        open: true
-    },
-    chainWebpack: config => {
-        config
-          .plugin('copy')
-          .tap(args => {
-            return [[{
-                from: 'src/public'
-            }]]
-          });
+    admins: {
+      entry: "src/admins.js",
+      filename: process.env.htmlPath + "admins.html"
     }
+  },
+  devServer: {
+    port: 88,
+    open: true,
+    proxy: "http://127.0.0.1:8000"
+  },
+  chainWebpack: config => {
+    config.plugin("copy").tap(args => {
+      return [
+        [
+          {
+            from: "src/public"
+          }
+        ]
+      ];
+    });
+  }
 };
